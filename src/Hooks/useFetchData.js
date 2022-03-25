d80c6a52cd3f584e1c323ec932f9645fca3f8ca0
Deeptikandel <deeptikandel1@gmail.com>
2022-03-25 15:23:59 +0200

    data fetch using axios

---
+++ src/Hooks/useFetchData.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const URL = "https://jsonplaceholder.typicode.com/users";
+
+const useFetchdata = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = axios
+      .get(URL)
+      .then((response) => setUsers(response.data))
+      .catch((err) => {
+        setError(err);
+      });
+    return () => {
+      fetchData();
+    };
+  }, []);
+  return { users, error };
+};
+export default useFetchdata;
---
+++ src/component/userDatails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserDatails = () => {
+  return (
+    <div>userDatails</div>
+  )
+}
+
+export default UserDatails
